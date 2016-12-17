447b869914208fd409b545acfbde953c287551ac
Элиана Русланова <erruslanova@edu.hse.ru>
2016-12-17 22:35:01 +0300

    Installing EntityFramework and adding Context class

---
+++ ShoppingCenter.Data/Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCenter.Data
+{
+    class Context : DbContext
+    {
+        public DbSet<Item> Items { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Journal> Journal { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
