d36beff107979b0828efdf030bbedff6dc5e5207
Элиана Русланова <erruslanova@edu.hse.ru>
2016-12-17 13:11:56 +0300

    New class 'Item' added

---
+++ ShoppingCenter.Data/Item.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCenter.Data
+{
+    class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+
+        public virtual decimal TotalPrice()
+        {
+            return Price;
+        }
+    }
+}
