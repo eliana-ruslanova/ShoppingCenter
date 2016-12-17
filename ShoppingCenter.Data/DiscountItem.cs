9e3feed57f9c7d5655191c6ae8d42490094ecef7
Элиана Русланова <erruslanova@edu.hse.ru>
2016-12-17 13:15:15 +0300

    New class DiscountItem added

---
+++ ShoppingCenter.Data/DiscountItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCenter.Data
+{
+    class DiscountItem : Item
+    {
+        public int Discount { get; set; }
+
+        public override decimal TotalPrice()
+        {
+            return base.TotalPrice() * (Discount/100);
+        }
+    }
+}
