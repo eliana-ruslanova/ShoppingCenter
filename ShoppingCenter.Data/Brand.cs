a196a3e2040626d3c09e9f894db9d7e562b7fcad
Элиана Русланова <erruslanova@edu.hse.ru>
2016-12-17 13:25:05 +0300

    New class Brand and one-to-many relationship between Brand and Item added

--- ShoppingCenter.Data/Brand.cs
+++ ShoppingCenter.Data/Brand.cs
@@ -6,18 +6,9 @@ using System.Threading.Tasks;
 
 namespace ShoppingCenter.Data
 {
-    class Item
+    class Brand
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public decimal Price { get; set; }
-        public int Quantity { get; set; }
-
-        public virtual decimal TotalPrice()
-        {
-            return Price;
-        }
-
-        public Brand Brand { get; set; }
     }
 }
