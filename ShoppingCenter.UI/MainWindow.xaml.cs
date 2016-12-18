56958855f67bb7608bf48eb72c265f6814d7167c
Valeria Vlasova <always394@mail.ru>
2016-12-18 14:21:19 +0300

    Renaming Journal to Invoices, adding Items list to the Department class, adding Invoices list to the Repository, adding event handler for Exit click to Main Window

--- ShoppingCenter.UI/MainWindow.xaml.cs
+++ ShoppingCenter.UI/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace ShoppingCenter.UI
         {
             InitializeComponent();
         }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
     }
 }
